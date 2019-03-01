9ad856098fb238deac9857f9436d045bf1b00da4
senzeyu <31408450+senzeyu@users.noreply.github.com>
2019-03-01 15:06:15 -0500

    Add files via upload

--- tcp_server_new-master/src/Server.java
+++ tcp_server_new-master/src/Server.java
@@ -5,9 +5,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.DecimalFormat;
+import java.time.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
+/*
+        LocalDateTime time = LocalDateTime.now();
+        String hour = new DecimalFormat("00").format(time.getHour());
+        String min = new DecimalFormat("00").format(time.getMinute());
+        String sec = new DecimalFormat("00").format(time.getSecond());
+        System.out.println(hour+min+sec);
+*/
         String serial_ID = "123";
         ServerSocket listener = new ServerSocket(9090);
         try{
@@ -40,9 +49,30 @@ public class Server {
                         out.write("3\n");
                         out.flush();
                     }
-                    else if (request.charAt(1) == 'u') {
-                        getDosageInfo dosage = new getDosageInfo();
-                        String dosageInfo = dosage.getInfo(serial_ID);
+                    else if (request.charAt(0) == 'u') {
+                      //  getDosageInfo dosage = new getDosageInfo();
+                      //  String dosageInfo = dosage.getInfo(serial_ID);
+                        System.out.println("Sending dosage info...");
+                        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        out.write("p0t1458n2t1120n3t2045n2p1t1459n1t1510n1t2210n5p2t1508n6t1505n6\n");
+                        out.flush();
+                        System.out.println("Sent");
+                    }
+                    else if(request.equals("time")){
+                        System.out.println("performed\n");
+                        LocalDateTime time = LocalDateTime.now();
+                        String hour = new DecimalFormat("00").format(time.getHour());
+                        String min = new DecimalFormat("00").format(time.getMinute());
+                        String sec = new DecimalFormat("00").format(time.getSecond());
+                        String time_message = hour+min+sec+'\n';
+                        System.out.println(time_message);
+
+                        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        out.write(time_message);
+                        out.flush();
+                    }
+                    else{
+
                     }
                 } finally {
                     socket.close();
