a2ea5d0add6755fb7b98766d8d1f8c4cf3f76d5c
senzeyu <31408450+senzeyu@users.noreply.github.com>
2019-03-24 17:02:10 -0400

    Add files via upload

--- tcp_server_new-master/src/Server.java
+++ tcp_server_new-master/src/Server.java
@@ -16,7 +16,7 @@ public class Server {
         String min = new DecimalFormat("00").format(time.getMinute());
         String sec = new DecimalFormat("00").format(time.getSecond());
         System.out.println(hour+min+sec);
-*/
+
         //send email test
         SendEmail sender = new SendEmail();
         //sender.send("001","01n3n5nn",'t');
@@ -25,10 +25,12 @@ public class Server {
         getDosageInfo dosagetest = new getDosageInfo();
         String dosageInfotest = dosagetest.getInfo("001");
         System.out.println(dosageInfotest);
-
+*/
         String serial_ID = "123";
+
         ServerSocket listener = new ServerSocket(9090);
         try{
+            String prevInfo = "\n";
             while(true){
                 Socket socket = listener.accept();
                 socket.setKeepAlive(true);
@@ -37,24 +39,8 @@ public class Server {
                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     String request = in.readLine();
                     System.out.println("Client request: " + request);
-                    if (request.equals("update")) {
-                        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-                        getDosageInfo dosage = new getDosageInfo();
-                        String dosageInfo = dosage.getInfo("001");
-                        String prevInfo = "\n";
-                        System.out.println(dosageInfo);
-                        if(prevInfo.equals(dosageInfo)){//avoid sending duplicate info to increase the efficiency
-                            out.write("n");
-                            out.flush();
-                        }else{
-                            out.write("p0t1517n1t1518n2t1519n3p1t1505n4t1506n5t2210n5p2t1508n6t1505n6\n");
-                            out.flush();
-                            prevInfo = dosageInfo;
-                        }
-                        System.out.println("Sent");
-                    }
-                    else if(request.equals("time")){
+                    if(request.equals("time")){
                         System.out.println("performed\n");
                         LocalDateTime time = LocalDateTime.now();
                         String hour = new DecimalFormat("00").format(time.getHour());
@@ -65,6 +51,30 @@ public class Server {
                         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                         out.write(time_message);
                         out.flush();
+                    }
+                    else if((request.length() > 6 && request.substring(0,6).equals("update")) ||
+                            (request.length() > 2 && request.substring(0,2).equals("fu"))) {//force update
+                        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        getDosageInfo dosage = new getDosageInfo();
+                        String dosageInfo = dosage.getInfo(request.substring(request.length() - 3, request.length()));
+
+                        //System.out.println("ID is " + request.substring(request.length() - 3, request.length()));
+                        System.out.println("prev: "+prevInfo);
+                        System.out.println("current: "+dosageInfo);
+
+                        if(request.substring(0,1).equals("fu")){//force update
+                            out.write(dosageInfo);
+                            out.flush();
+                        }
+                        else if(prevInfo.equals(dosageInfo)){//avoid sending duplicate info to increase the efficiency
+                            out.write("n");
+                            out.flush();
+                        }else{
+                            out.write(dosageInfo);
+                            out.flush();
+                            prevInfo = dosageInfo;
+                        }
+
                     }
                     else if (request.length() >= 10) {
                         if (request.charAt(10) == 't' || request.charAt(10) == 'f'){/*
@@ -77,11 +87,15 @@ public class Server {
                             String seg_IDs = request.substring(0, 10);
                             System.out.println("Segmets require operations are:" + seg_IDs);
                             String email_address = mail.get(serial_ID);
+                            if(!seg_IDs.equals("nnnnnnnnnn")){
+                                SendEmail sender = new SendEmail();
+                                sender.send(serial_ID,seg_IDs,request.charAt(10));
+                            }
 
-                            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-                            System.out.println("Sending Message to Client...");
-                            out.write("5\n");
-                            out.flush();
+                          //  BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                          //  System.out.println("Sending Message to Client...");
+                          //  out.write("5\n");
+                          //  out.flush();
                         }
                     }
                     else{
